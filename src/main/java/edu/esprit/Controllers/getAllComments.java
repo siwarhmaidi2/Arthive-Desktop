8d8df2e4e752b63be25a48020bd50f424fa8ea02
toujaniayoub <ayoub.toujani@esprit.tn>
2024-02-27 12:00:41 +0100

    i added to the comments controller the handle likes methods, handle update ,handle delete

--- src/main/java/edu/esprit/Controllers/getAllComments.java
+++ src/main/java/edu/esprit/Controllers/getAllComments.java
@@ -1,23 +1,31 @@
 package edu.esprit.Controllers;
 
+import edu.esprit.entities.ReactionCommentaire;
 import edu.esprit.entities.User;
 import edu.esprit.services.ServiceCommentaire;
+import edu.esprit.services.ServicePublication;
+import edu.esprit.services.ServiceReactionCommentaire;
 import edu.esprit.services.ServiceUser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import edu.esprit.entities.Commentaire;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 public class getAllComments {
 
+
+    @FXML
+    private MenuItem updateComment;
+    @FXML
+    private MenuItem deleteComment;
     @FXML
     private VBox comment;
 
@@ -33,11 +41,41 @@ public class getAllComments {
     @FXML
     private Label username;
 
+    @FXML
+    private ImageView iconLikes;
+
+    @FXML
+    private Button likeComment;
+
+    @FXML
+    private Label nbLikesComments;
+
+    @FXML
+    private Button saveCommentButton;
+//    @FXML
+//    private VBox repliesContainer;
+//
+//    @FXML
+//    private TextArea replyTextArea;
+//
+//    @FXML
+//    private Button submitReplyButton;
     private Commentaire commentaire;
 
     private ServiceCommentaire serviceCommentaire = new ServiceCommentaire();
+    private ServiceReactionCommentaire serviceReactionCommentaire = new ServiceReactionCommentaire();
+
+    private ServicePublication servicePublication = new ServicePublication();
+
+
     private ServiceUser serviceUser = new ServiceUser();
 
+    int currentPublicationId = 37;
+
+    @FXML
+    void initialize() {
+
+    }
     public void setData(Commentaire commentaire) {
         try {
             this.commentaire = commentaire;
@@ -50,23 +88,115 @@ public class getAllComments {
                 this.username.setText(commentaire.getUser().getNom_user() + " " + commentaire.getUser().getPrenom_user());
                 // profileImage.setImage(commentaire.getUser().getPhoto_user());
             }
+            updateLikeCountLabel();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+// add a like on a comment and remove it if it's already liked
     @FXML
-    void handleEditComment(ActionEvent event) {
+    void handleLikeComment(ActionEvent event) {
+
         // Assuming you have a method to get the logged-in user
         User loggedInUser = serviceUser.authenticateUser("ayoubtoujani808@gmail.com", "1234563");
 
-        // Check if the logged-in user is the owner of the comment
-        if (commentaire.getUser().equals(loggedInUser)) {
-            // Enable editing for the TextArea
-            content.setEditable(true);
+        // Check if the user has already liked this comment
+        if (!hasUserLikedComment(loggedInUser)) {
+            // Create a new ReactionCommentaire representing the like
+            ReactionCommentaire like = new ReactionCommentaire();
+            like.setCommentaire(serviceCommentaire.getOneByID(commentaire.getIdCommentaire()));
+            like.setUser(loggedInUser);
+
+
+            like.setPublication(servicePublication.getOneByID(currentPublicationId));
+
+            like.setDateAjoutReactionCommentaire(new java.sql.Timestamp(System.currentTimeMillis()));
+
+
+            // Add the like using the ServiceReactionCommentaire
+            serviceReactionCommentaire.add(like);
+
+            // Update the like count label
+            updateLikeCountLabel();
         } else {
-            showAlert("You don't have permission to edit this comment.");
+            removeLike(loggedInUser);
+
+            // Update the like count label
+            updateLikeCountLabel();
         }
     }
+    private void removeLike(User user) {
+        // Find and remove the like from the database
+        ReactionCommentaire likeToRemove = null;
+        for (ReactionCommentaire reaction : serviceReactionCommentaire.getAll()) {
+            if (reaction.getUser().equals(user) && reaction.getCommentaire().equals(commentaire)) {
+                likeToRemove = reaction;
+                break;
+            }
+        }
+
+        if (likeToRemove != null) {
+            // Remove the like using the ServiceReactionCommentaire
+            serviceReactionCommentaire.delete(likeToRemove.getIdReactionCommentaire());
+        }
+    }
+    private boolean hasUserLikedComment(User user) {
+        // Check if the user has already liked this comment
+        for (ReactionCommentaire reaction : serviceReactionCommentaire.getAll()) {
+            if (reaction.getUser().equals(user) && reaction.getCommentaire().equals(commentaire)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateLikeCountLabel() {
+        if (commentaire == null) {
+            nbLikesComments.setText("0"); // or handle it as appropriate
+            return;
+        }
+
+        // Get the updated like count from the database
+        int likeCount = serviceReactionCommentaire.getLikesCountForComment(commentaire.getIdCommentaire());
+
+        // Update the like count label
+        nbLikesComments.setText(String.valueOf(likeCount));
+    }
+
+    @FXML
+    void handleEditComment(ActionEvent event) {
+        // Enable editing in the TextArea
+
+        User loggedInUser = serviceUser.authenticateUser("ayoubtoujani808@gmail.com", "1234563");
+        if (commentaire.getUser().equals(loggedInUser)) {
+            content.setEditable(true);
+
+            // Change the appearance to indicate editing is active
+            content.setStyle("-fx-border-color: blue; -fx-border-width: 2px;");
+            saveCommentButton.setVisible(true);
+            // You can also change other styles as needed, such as background color or font style.
+        }
+    }
+
+    @FXML
+    void saveEditedComment(ActionEvent event) {
+        // Disable editing in the TextArea
+        content.setEditable(false);
+
+        // Save the edited content to the database
+        String editedContent = content.getText();
+        commentaire.setContenuCommentaire(editedContent);
+        serviceCommentaire.update(commentaire);
+
+        // Change the appearance to indicate editing is complete
+        content.setStyle(""); // Reset styles to default
+        saveCommentButton.setVisible(false);
+        // You may also want to update other UI elements or change the appearance to indicate that editing is complete.
+    }
+
+
+// edit and delete a comment
+
     @FXML
     void handleDeleteComment(ActionEvent event) {
         // Assuming you have a method to get the logged-in user
@@ -94,6 +224,9 @@ public class getAllComments {
             showAlert("You don't have permission to delete this comment.");
         }
     }
+
+// Other methods...
+
 
     private void removeCommentNode() {
         // Access the parent VBox from the comment node and remove it
